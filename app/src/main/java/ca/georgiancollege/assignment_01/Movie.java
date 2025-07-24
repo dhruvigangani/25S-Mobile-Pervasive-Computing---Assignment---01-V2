852fca67847a4e5b942b766e1bde838ff5558b9a
dhruvigangani <dhruvig2020@gmail.com>
2025-07-24 13:57:04 -0400

    created contructor and getters method for each

--- app/src/main/java/ca/georgiancollege/assignment_01/Movie.java
+++ app/src/main/java/ca/georgiancollege/assignment_01/Movie.java
@@ -7,5 +7,34 @@ public class Movie {
     private double rating;
     private double imdbID;
 
+    //contructor method
+    public Movie(String title, String director, int year, double rating, double imdbID) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+        this.imdbID = imdbID;
+    }
+
+    //getter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getImdbID() {
+        return imdbID;
+    }
 
 }
