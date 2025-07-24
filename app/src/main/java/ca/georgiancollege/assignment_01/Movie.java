9c379a8317dac6d55cf310a73e3ae27a07897505
dhruvigangani <dhruvig2020@gmail.com>
2025-07-24 13:54:46 -0400

    created movie.java and added model class

---
+++ app/src/main/java/ca/georgiancollege/assignment_01/Movie.java
@@ -0,0 +1,11 @@
+package ca.georgiancollege.assignment_01;
+
+public class Movie {
+    private String title;
+    private String director;
+    private int year;
+    private double rating;
+    private double imdbID;
+
+
+}
