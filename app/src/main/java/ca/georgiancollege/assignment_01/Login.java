1c637d9ccf186234dac5465b110f059ed5826695
dhruvigangani <dhruvig2020@gmail.com>
2025-07-31 19:03:47 -0400

    Created login activity

---
+++ app/src/main/java/ca/georgiancollege/assignment_01/Login.java
@@ -0,0 +1,24 @@
+package ca.georgiancollege.assignment_01;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ca.georgiancollege.assignment_01.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
